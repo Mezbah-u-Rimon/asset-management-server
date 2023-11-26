95019be35012e9c4c37659f6d67a4dddff7e6c87
Muzbah Uddin <rimonmr444@gmail.com>
2023-11-27 01:15:45 +0600

    create adminUserCollection and employeeusercollection and post the data in server side

--- index.js
+++ index.js
@@ -29,6 +29,40 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+        const adminUserCollection = await client.db("assetDB").collection("adminUsers")
+        const employeeUserCollection = await client.db("assetDB").collection("employeeUsers")
+
+
+        //admin user collection
+        app.post('/adminUsers', async (req, res) => {
+            const user = req.body;
+
+            const query = { email: user.email }
+            const existingUser = await adminUserCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User already exists', insertedId: null })
+            }
+            const result = await adminUserCollection.insertOne(user);
+            res.send(result)
+        })
+
+
+
+        //employee user collection
+        app.post('/employeeUsers', async (req, res) => {
+            const user = req.body;
+
+            const query = { email: user.email }
+            const existingUser = await employeeUserCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User already exists', insertedId: null })
+            }
+            const result = await employeeUserCollection.insertOne(user);
+            res.send(result)
+        })
+
+
+
 
 
         //payment method
